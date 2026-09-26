006e96bfbea1a924489224ba6fb63976d8d26b28
Hamza Outayb <houtayb@gmail.com>
2026-09-26 11:10:08 +0000

    working with .env

--- src/main/java/com/example/lets_play/config/AdminSeeder.java
+++ src/main/java/com/example/lets_play/config/AdminSeeder.java
@@ -6,9 +6,19 @@ import org.springframework.stereotype.Component;
 import com.example.lets_play.model.Role;
 import com.example.lets_play.model.User;
 import com.example.lets_play.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 public class AdminSeeder {
+
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -24,9 +34,9 @@ public class AdminSeeder {
             if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
                 User admin = new User();
                 admin.setRole(Role.ADMIN);
-                admin.setName("admin");
-                admin.setEmail("admin@gmail.com");
-                admin.setPassword(passwordEncoder.encode("admin123456789"));
+                admin.setName(adminUsername);
+                admin.setEmail(adminEmail);
+                admin.setPassword(passwordEncoder.encode(adminPassword));
                 userRepository.save(admin);
                 System.out.println(" Admin user created successfully!");
             } else {
